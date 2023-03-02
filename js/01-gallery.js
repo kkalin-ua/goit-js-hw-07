9a410c7fcaaf80980de1953430e213a68d2ca8ab
kkalin-ua <94871840+kkalin-ua@users.noreply.github.com>
2023-03-02 13:31:42 +0200

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,7 +25,6 @@
 
 
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox';
 
 const imgEl= galleryItems
   .map(({preview, original, description}) => `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description} /></a></div>`)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,6 +12,9 @@
 // відкриття зображення.
 
 
+
+
+// // **********************************
 import { galleryItems } from './gallery-items.js';
 // import SimpleLightbox from "simplelightbox";
 // import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
@@ -39,13 +42,31 @@ const imgEl= galleryItems
 
 
 
+// // **********************************
 
 
 
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
+// const gallery = document.querySelector('.gallery');
 
+// gallery.innerHTML = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `
+//       <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>
+//     `;
+//   })
+//   .join('');
 
+//  new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
 
+// console.log(galleryItems);
 
 
 
